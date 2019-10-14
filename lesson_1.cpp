381e40b0ae7e117d2e33181c206b5bcc2596f2d2
Hyppocrat <djsh92@mail.ru>
2019-10-14 20:05:03 +0300

    Update lesson_1.cpp

--- lesson_1.cpp
+++ lesson_1.cpp
@@ -1,4 +1,18 @@
+// стандартная шапка
+
 #include <iostream>
 using namespace std;
 int main(){
+}
+
+========================
+  
+// программа нахождения суммы двух чисел
+#include <iostream>
+using namespace std;
+int main(){
+  int a, b;
+  cin >> a >> b;
+  int c = a + b;
+  cout << c;
 }
