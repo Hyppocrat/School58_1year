bf722c286f948a7865f15fd8a1195286808e8bc4
Hyppocrat <djsh92@mail.ru>
2019-10-28 13:46:16 +0300

    Update lesson_2.cpp

--- lesson_2.cpp
+++ lesson_2.cpp
@@ -1,3 +1,45 @@
+//найти максимальное из 2 чисел
+#include <iostream>
+using namespace std;
+int main(){
+  int a, b;
+  cin >> a >> b;
+  if (a > b){
+    cout << a;
+  }
+  else {
+    cout << b;
+  }
+}
+
+//проверить число на двузначность. способ 1
+#include <iostream>
+using namespace std;
+int main(){
+  int a;
+  cin >> a;
+  if ((a > 9) && (a < 100)){
+    cout << "YES";
+  }
+  else {
+    cout << "NO";
+  }
+}
+
+//проверить число на двузначность. способ 2
+#include <iostream>
+using namespace std;
+int main(){
+  int a;
+  cin >> a;
+  if ((a <= 9) || (a >= 100)){
+    cout << "YES";
+  }
+  else {
+    cout << "NO";
+  }
+}
+
 //упорядочить три числа по возрастанию
 #include <iostream>
 using namespace std;
